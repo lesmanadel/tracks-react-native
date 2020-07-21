b4d71d1f4fad99ca9c398e8cc822d08555a8b827
lesmanadel <lesmanadel3@gmail.com>
2020-07-21 22:22:54 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+import AccountScreen from './src/screens/AccountScreen';
+import SigninScreen from './src/screens/SigninScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import TrackCreateScreen from './src/screens/TrackCreateScreen';
+import TrackDataScreen from './src/screens/TrackDataScreen';
+import TrackListScreen from './src/screens/TrackListScreen';
+
+import { AppRegistry } from 'react-native';
+AppRegistry.registerComponent('track', ()  => App);
---
+++ src/screens/TrackDataScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, StyleSheet, Text } from 'react-native';
+
+const TrackDataScreen = () => {
+  return <Text style={{ fontSize: 48 }}>TrackDataScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default TrackDataScreen;
